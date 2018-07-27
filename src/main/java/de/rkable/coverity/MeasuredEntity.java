8f3f7c45bd3caaadb0f25d5824cd4b74ec93b310
MarkDrei <mark@mopedoversum.de>
2018-07-27 15:26:35 +0200

    Updated project structure
    now matches the default gradle style and uses junit 5

---
+++ src/main/java/de/rkable/coverity/MeasuredEntity.java
@@ -0,0 +1,5 @@
+package de.rkable.coverity;
+
+public class MeasuredEntity {
+
+}
---
+++ src/test/java/de/rkable/coverity/MeasuredEntityTest.java
@@ -0,0 +1,14 @@
+package de.rkable.coverity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MeasuredEntityTest {
+
+	@Test
+	public void testMeasuredEntity() {
+		new MeasuredEntity();
+		assertTrue(true);
+	}
+}
